7fc0b0e8aaa33ba0bd0f6fb494f074330726b4ee
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-12-31 18:36:15 -0500

    adds sidebar close on selected menu

--- src/components/MainLayout/SidebarMenuOptions.tsx
+++ src/components/MainLayout/SidebarMenuOptions.tsx
@@ -1,32 +1,44 @@
 import React from "react";
+// jotai
+import { useAtom } from "jotai";
+import { isSidebarOpenAtom } from "src/store/MainLayoutStore";
 // next-router
 import { useRouter } from "next/router";
 // mui
 import { Typography } from "@mui/material";
 
 export default function SidebarMenuOptions() {
+  const [, setIsSidebarOpen] = useAtom(isSidebarOpenAtom);
   const router = useRouter();
+
+  const handleMenuClick = (stringRoute: string) => {
+    router.push(stringRoute);
+    setIsSidebarOpen(false);
+  };
 
   return (
     <div className="flex flex-col gap-5 justify-center h-full w-full">
-      <Typography sx={{ cursor: "pointer" }} onClick={() => router.push("/")}>
+      <Typography
+        sx={{ cursor: "pointer" }}
+        onClick={() => handleMenuClick("/")}
+      >
         Overall
       </Typography>
       <Typography
         sx={{ cursor: "pointer" }}
-        onClick={() => router.push("/byboardgames")}
+        onClick={() => handleMenuClick("/byboardgames")}
       >
         By Board Games
       </Typography>
       <Typography
         sx={{ cursor: "pointer" }}
-        onClick={() => router.push("/addstat")}
+        onClick={() => handleMenuClick("/addstat")}
       >
         Add Stat
       </Typography>
       <Typography
         sx={{ cursor: "pointer" }}
-        onClick={() => router.push("/managestats")}
+        onClick={() => handleMenuClick("/managestats")}
       >
         Manage Stats
       </Typography>
