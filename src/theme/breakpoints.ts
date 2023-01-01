86bab82bdfbb80b58e36e572affc2241652f33ec
import-turtle <104483060+import-turtle@users.noreply.github.com>
2023-01-01 09:34:58 -0500

    adds more theme customization

---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,13 @@
+const BREAKPOINTS = {
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 370,
+      md: 760,
+      lg: 1200,
+      xl: 1920,
+    },
+  },
+};
+
+export default BREAKPOINTS;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,12 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 //
 import PALETTE from "src/theme/palette";
 import TYPOGRAPHY from "src/theme/typography";
 import COMPONENTS from "./components";
+import BREAKPOINTS from "./breakpoints";
 
-const theme = createTheme({ ...PALETTE, ...TYPOGRAPHY, ...COMPONENTS });
+const theme = responsiveFontSizes(
+  createTheme({ ...PALETTE, ...TYPOGRAPHY, ...COMPONENTS, ...BREAKPOINTS })
+);
 
 export default theme;
