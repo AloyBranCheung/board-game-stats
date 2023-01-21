f0a5c3d7b3b4d388550ab24ae991b0ecae386c2a
import-turtle <104483060+import-turtle@users.noreply.github.com>
2023-01-20 19:34:58 -0500

    starts wingspan calculator

---
+++ pages/wingspancalculator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//
+import WingspanCalculatorContainer from "src/components/WingspanCalculatorContainer";
+
+export default function WingspanCalculator() {
+  return <WingspanCalculatorContainer />;
+}
