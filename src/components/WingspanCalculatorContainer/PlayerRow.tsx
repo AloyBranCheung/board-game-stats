9f4a7f80b5f8f28bb6c6ce9ad2e92d5d7306ac92
Brandon <brandon.cheung.dev@gmail.com>
2023-03-21 18:09:53 -0400

    init

--- src/components/WingspanCalculatorContainer/PlayerRow.tsx
+++ src/components/WingspanCalculatorContainer/PlayerRow.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+// mui
+import { Card, Box } from "@mui/material";
 
 export default function PlayerRow() {
-  return <div>PlayerRow</div>;
+  return (
+    <Card sx={{ padding: "1.25rem" }}>
+      <Box>PlayerRow</Box>
+    </Card>
+  );
 }
--- src/components/WingspanCalculatorContainer/index.tsx
+++ src/components/WingspanCalculatorContainer/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 // components
 import PlayerRow from "./PlayerRow";
 // mui
-import { Container, Card } from "@mui/material";
+import { Container } from "@mui/material";
 
 export default function WingspanCalculatorContainer() {
   return (
     <Container sx={{ paddingTop: "1.25rem", paddingBottom: "5rem" }}>
-      <Card sx={{ padding: "1.25rem" }}>
-        <PlayerRow />
-      </Card>
+      <PlayerRow />
     </Container>
   );
 }
