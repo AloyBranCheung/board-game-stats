ee40d2682f4d3b7fe90d07775bd6d42142d6cff0
Brandon <brandon.cheung.dev@gmail.com>
2023-03-22 22:05:40 -0400

    init socket container level

--- src/components/WingspanCalculatorContainer/index.tsx
+++ src/components/WingspanCalculatorContainer/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 // components
 import PlayerRow from "./PlayerRow";
 // mui
 import { Container } from "@mui/material";
+// hooks
+import useSocketIo from "src/hooks/useSocketIo";
 
 export default function WingspanCalculatorContainer() {
+  const socket = useSocketIo();
+
+  // useEffect to attach socket listeners
+
   return (
     <Container sx={{ paddingTop: "1.25rem", paddingBottom: "5rem" }}>
       <PlayerRow />
