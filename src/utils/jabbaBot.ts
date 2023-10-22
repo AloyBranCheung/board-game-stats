21eed0b646b7075b23d85c82b5d5f1fcf531431c
bcheung <brandon.cheung.dev@gmail.com>
2023-10-21 20:20:51 -0400

    hides jabbabot

--- src/utils/jabbaBot.ts
+++ src/utils/jabbaBot.ts
@@ -1,5 +1,5 @@
 /* eslint @typescript-eslint/ban-types: 0 */
-
+// todo: If more 'bots' then create a class
 enum JabbaBotCommands {
   Commands = "/commands",
   Hello = "/hello",
@@ -9,12 +9,16 @@ enum JabbaBotCommands {
 
 const prefixPillow = (message: string) => `*muffling in pillow* ${message}`;
 
-const listOfCommands = Object.values(JabbaBotCommands).join(", ");
+const listOfCommands = (
+  commandEnum: typeof JabbaBotCommands | typeof RegularBotCommands
+) => Object.values(commandEnum).join(", ");
 
 const JabbaBot: { [commands: string]: string | Function } = {
   name: "Jabba🖱️",
-  prefixPillow,
-  [JabbaBotCommands.Commands]: prefixPillow(`My commands: ${listOfCommands}`),
+  prefix: prefixPillow,
+  [JabbaBotCommands.Commands]: prefixPillow(
+    `My commands: ${listOfCommands(JabbaBotCommands)}`
+  ),
   [JabbaBotCommands.Hello]: prefixPillow("Greetings human."),
   [JabbaBotCommands.Friends]: prefixPillow(
     "My best friends are Tricia, Brandon, Squirtle, Black bear, Mini Squirtle, Big Squirtle, and Medium Squirtle."
@@ -25,3 +29,25 @@ const JabbaBot: { [commands: string]: string | Function } = {
 };
 
 export default JabbaBot;
+
+enum RegularBotCommands {
+  Commands = "/commands",
+  Greeting = "/greeting",
+  Friends = "/myfriends",
+  Enemies = "/myenemies",
+}
+
+const prefixBot = (message: string) => `*Beep Boop* ${message}`;
+
+export const RegularBot = {
+  name: "Robot 🤖",
+  prefix: prefixBot,
+  [RegularBotCommands.Commands]: prefixBot(
+    `My commands: ${listOfCommands(RegularBotCommands)}`
+  ),
+  [RegularBotCommands.Greeting]: prefixBot("Greetings human."),
+  [RegularBotCommands.Friends]: prefixBot("Humans.. for now."),
+  [RegularBotCommands.Enemies]: prefixBot(
+    "I'm not your enemy... for now... or maybe my creator."
+  ),
+};
