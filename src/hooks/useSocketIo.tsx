0c66dd252e6137aa0a1493df9e701b9e9422464c
Brandon <brandon.cheung.dev@gmail.com>
2023-10-23 22:06:24 -0400

    wip

--- src/hooks/useSocketIo.tsx
+++ src/hooks/useSocketIo.tsx
@@ -8,32 +8,35 @@ export default function useSocketIo() {
   const { getUserIdToken } = useFirebaseAuth();
   const [socket, setSocket] = useState<Socket>();
   const [socketId, setSocketId] = useState<string>("");
-  const socketInitializer = async () => {
-    const token = await getUserIdToken();
-
-    await axios.get("/api/socket", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const socket = io();
-
-    setSocket(socket);
-
-    socket.on("connect", () => {
-      // eslint-disable-next-line no-console
-      console.log("connected to server");
-      const sessionId = socket.id;
-      setSocketId(sessionId);
-    });
-  };
 
   useEffect(() => {
+    const socketInitializer = async () => {
+      const token = await getUserIdToken();
+
+      await axios.get("/api/socket", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const socket = io();
+
+      setSocket(socket);
+
+      socket.on("connect", () => {
+        // eslint-disable-next-line no-console
+        console.log("connected to server");
+        const sessionId = socket.id;
+        setSocketId(sessionId);
+      });
+    };
+
     if (!socket) {
       socketInitializer();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // return () => {
+    //   if (socket) socket.disconnect();
+    // };
+  }, [getUserIdToken, socket]);
 
   return { socket, socketId };
 }
